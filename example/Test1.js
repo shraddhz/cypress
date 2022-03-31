b1f3b19b2fcb55ab176d8a111f5b58b441bd44db
Shraddhz S <shraddhzs@shraddhz.com>
2022-03-31 12:11:51 +0530

    initial comment

---
+++ example/Test1.js
@@ -0,0 +1,13 @@
+describe('My Frist Try', function()
+{
+    it('My Frist Test', function()
+    {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        
+    })
+    it('My Second Test', function()
+    {
+
+        
+    })
+})
